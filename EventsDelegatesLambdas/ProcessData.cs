6a13f29aac54a96ed11e252f20872516d8a84b62
Ramon Felipe Alves de Arruda Silva <ramon.felipe@hotmail.com>
2022-06-18 20:40:30 -0300

    using lambdas with custom delegate

---
+++ EventsDelegatesLambdas/ProcessData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDelegatesLambdas
+{
+    public class ProcessData
+    {
+        public int Process(int x, int y, BizRulesDelegate del)
+        {
+            var result = del(x, y);
+
+            return result;
+        }
+    }
+}
--- EventsDelegatesLambdas/Program.cs
+++ EventsDelegatesLambdas/Program.cs
@@ -2,72 +2,99 @@
 
 using EventsDelegatesLambdas;
 
-static void WorkPerformed1(WorkPerformedEventArgs e) =>
+Method1();
+Method2();
+Method3();
+
+
+
+static void Method1()
+{
+    static void WorkPerformed1(WorkPerformedEventArgs e) =>
     Console.WriteLine("WorkPerformed1. Worker performed {0} for {1} hours", e.WorkType, e.Hours);
 
-static void WorkPerformed2(WorkPerformedEventArgs e) => 
-    Console.WriteLine("WorkPerformed2. Worker performed {0} for {1} hours", e.WorkType, e.Hours);
+    static void WorkPerformed2(WorkPerformedEventArgs e) =>
+        Console.WriteLine("WorkPerformed2. Worker performed {0} for {1} hours", e.WorkType, e.Hours);
 
-static void WorkPerformed3(WorkPerformedEventArgs e) => 
-    Console.WriteLine("WorkPerformed3. Worker performed {0} for {1} hours", e.WorkType, e.Hours);
+    static void WorkPerformed3(WorkPerformedEventArgs e) =>
+        Console.WriteLine("WorkPerformed3. Worker performed {0} for {1} hours", e.WorkType, e.Hours);
 
-static void WorkCompleted(object? sender, EventArgs e) => 
-    Console.WriteLine("WORK COMPLETED!!");
-
-
-var delArgs = new WorkPerformedEventArgs(2, WorkType.Golf); 
-
-Console.WriteLine("Hello, World!");
-
-var del1 = WorkPerformed1;
-var del2 = WorkPerformed2;
-var del3 = WorkPerformed3;
-var del4 = new EventHandler(WorkCompleted);
-
-del1 += del2 + del3;
-del1 += (args) => Console.WriteLine("Last call del1 lambda...");
-
-var worker = new Worker(del1, del4);
-worker.DoWork(null, delArgs);
+    static void WorkCompleted(object? sender, EventArgs e) =>
+        Console.WriteLine("WORK COMPLETED!!");
 
 
-var w2 = new Worker();
-w2.WorkPerformed += Work_OnWorkPerformed;
-w2.WorkCompleted += Work_OnWorkCompleted;
-w2.WorkCompletedWithNoArgs += Work_OnWorkCompletedWithNoArgs;
-w2.WorkCompletedWithNoArgs += () =>
-{
-    Console.WriteLine("testing...");
-};
+    var delArgs = new WorkPerformedEventArgs(2, WorkType.Golf);
 
-w2.DoWork(null, delArgs);
+    Console.WriteLine("Hello, World!");
 
-static void Work_OnWorkPerformed(WorkPerformedEventArgs args)
-{
-    Console.WriteLine("Performing work. From Program...");
+    var del1 = WorkPerformed1;
+    var del2 = WorkPerformed2;
+    var del3 = WorkPerformed3;
+
+    var del4 = new EventHandler(WorkCompleted);
+
+    del1 += del2 + del3;
+    del1 += (args) => Console.WriteLine("Last call del1 lambda...");
+
+    var worker = new Worker(del1, del4);
+    worker.DoWork(null, delArgs);
 }
 
-static void Work_OnWorkCompleted(object? sender, EventArgs args)
+static void Method2()
 {
-    Console.WriteLine("Work Completed! From Program...");
+    var delArgs = new WorkPerformedEventArgs(2, WorkType.Golf);
+
+    var w2 = new Worker();
+    w2.WorkPerformed += Work_OnWorkPerformed;
+    w2.WorkCompleted += Work_OnWorkCompleted;
+    w2.WorkCompletedWithNoArgs += Work_OnWorkCompletedWithNoArgs;
+    w2.WorkCompletedWithNoArgs += () =>
+    {
+        Console.WriteLine("testing...");
+    };
+
+    w2.DoWork(null, delArgs);
+
+    static void Work_OnWorkPerformed(WorkPerformedEventArgs args)
+    {
+        Console.WriteLine("Performing work. From Program...");
+    }
+
+    static void Work_OnWorkCompleted(object? sender, EventArgs args)
+    {
+        Console.WriteLine("Work Completed! From Program...");
+    }
+
+    static void Work_OnWorkCompletedWithNoArgs()
+    {
+        Console.WriteLine("Work Completed With No Args! From Program...");
+    }
+
+    AddDelegate ad = (a, b) => a + b;
+    var result = ad(1, 10);
+    Console.WriteLine("Resultado: {0}", result);
+
+
+    Func<bool> BoolFunc = () =>
+    {
+        Console.WriteLine("Testing func...");
+        return true;
+    };
+
+    Console.WriteLine("Resultado BoolFunc: {0}", BoolFunc());
 }
 
-static void Work_OnWorkCompletedWithNoArgs()
+
+static void Method3()
 {
-    Console.WriteLine("Work Completed With No Args! From Program...");
+    var processData = new ProcessData();
+    BizRulesDelegate delAdd = (int x, int y) => x + y;
+    BizRulesDelegate delMult = (int x, int y) => x * y;
+
+    Console.WriteLine($"{nameof(BizRulesDelegate)}: ({nameof(delAdd)}): " + processData.Process(2, 3, delAdd));
+    Console.WriteLine($"{nameof(BizRulesDelegate)}: ({nameof(delMult)}): " + processData.Process(2, 3, delMult));
 }
-
-AddDelegate ad = (a, b) => a + b;
-var result = ad(1, 10);
-Console.WriteLine("Resultado: {0}", result);
-
-
-Func<bool> BoolFunc = () =>
-{
-    Console.WriteLine("Testing func...");
-    return true;
-};
-
-Console.WriteLine("Resultado BoolFunc: {0}", BoolFunc());
 
 public delegate int AddDelegate(int a, int b);
+public delegate int BizRulesDelegate(int x, int y);
+
